76811ceb435464be7211d96955f0ad5d17516a97
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-10-08 16:43:24 -0700

    Single page site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Responsive Nav
 const navSlide = () => {
   const all = document.querySelector('.all');
   const nav = document.querySelector('.nav-links');
@@ -11,33 +10,31 @@ const navSlide = () => {
     nav.classList.toggle('nav-active');
   });
 
-  // current link style
   for (i = 0; i < links.length; i++) {
-    // attach event listener to all links
+
     links[i].addEventListener('click', clickMyLink);
   }
-  // event listener function
+
   function clickMyLink(e) {
-    //Call Smooth Scroll Function
+
     smoothScroll(event);
 
     if (nav.classList.contains('nav-active')) {
-      // Close navbarMenu in smaller screens
       all.click();
     }
 
     for (j = 0; j < section.length; j++) {
-      // deactivate all links
+     
       links[j].classList.remove('current');
-      // hide all section
+      
       section[j].classList.remove('current');
     }
-    // activate current myLink
+   
     e.currentTarget.classList.add('current');
   }
-  // smooth scrolling
+  
   function smoothScroll(event) {
-    // console.log(event.target);
+    
     event.preventDefault();
     const targetId = event.currentTarget.getAttribute('href');
     let destination = document.querySelector(targetId).offsetTop;
@@ -47,7 +44,6 @@ const navSlide = () => {
     });
   }
 
-  // Scroll Up / Back To Top Image
   const scrollBtn = document.getElementById('scrollUp');
   const section1 = document.getElementById('section1');
   scrollBtn.addEventListener('click', e => {
